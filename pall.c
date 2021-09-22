74791c4bd94f0f88cc44a353a16c3a9d8fa68dc7
firdaushassansalim <firdaushassan033@gmail.com>
2021-09-22 18:10:55 +0300

    tried task 1

---
+++ pall.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * pall - prints the stack
+ * @stack: stack given by main
+ * @line_cnt: amount of lines
+ *
+ * Return: void
+ */
+void pall(stack_t **stack, unsigned int line_cnt __attribute__((unused)))
+{
+	print_stack(*stack);
+}
+
