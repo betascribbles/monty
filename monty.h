8b42a715191b833ddfd111aeecf93bde0bc16a4e
enzonjagi <amonsdungo@gmail.com>
2021-09-22 11:57:02 +0300

    Betty correcting monty.h

--- monty.h
+++ monty.h
@@ -1,5 +1,5 @@
-#ifndef  MONTY_H
-#define MONTY_H 
+#ifndef MONTY_H
+#define MONTY_H
 
 #include <stdio.h>
 
@@ -14,9 +14,9 @@
  */
 typedef struct stack_s
 {
-      int n;
-      struct stack_s *prev;
-      struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -29,8 +29,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-      char *opcode;
-      void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 
