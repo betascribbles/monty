c14cb9ede4a971234e8fcfaa816d20ee182c3b8e
enzonjagi <amonsdungo@gmail.com>
2021-09-23 11:43:19 +0300

    Did some debugging and still wondering about what the INSTRUCTIONS part of the code means

--- push.c
+++ push.c
@@ -15,28 +15,34 @@ void push(stack_t **stack, unsigned int line_cnt)
 {
 	char *n = global.argument;
 
-	if ((is_digit(n)) == 0)
+	if ((isdigit(n)) == 0)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_cnt);
-		status = EXIT_FAILURE;
-		return;
+		/*status = EXIT_FAILURE;*/
+		exit(EXIT_FAILURE);
+		/*return;*/
 	}
 
 	if (global.data_struct == 1)
 	{
 		if (!add_node(stack, atoi(global.argument)))
 		{
-			return;
-			status = EXIT_FAILURE;
+			/*
+			 *return;
+			 *status = EXIT_FAILURE;
+			 */
+			exit(EXIT_FAILURE);
 		}
 	}
 	else
 	{
 		if (!queue_node(stack, atoi(global.argument)))
 		{
-			return;
-			status = EXIT_FAILURE;
+			/*
+			*return;
+			*status = EXIT_FAILURE;
+			*/
+			exit(EXIT_FAILURE);
 		}
 	}
 }
-
