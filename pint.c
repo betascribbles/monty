45f8eef40176fe735effb92651aae92197f3213a
firdaushassansalim <firdaushassan033@gmail.com>
2021-09-23 21:07:56 +0300

    fixed bug

--- pint.c
+++ pint.c
@@ -15,8 +15,8 @@ void pint(stack_t **stack, unsigned int line_cnt)
 	if (!stack || !(*stack))
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_cnt);
-		status = EXIT_FAILURE;
-		return;
+		exit (EXIT_FAILURE);
+		
 	}
 	printf("%d\n", (*stack)->n);
 }
