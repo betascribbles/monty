e68422e6449c4f3aee8db8111bbc0ffd3b454de7
firdaushassansalim <firdaushassan033@gmail.com>
2021-09-25 15:44:00 +0300

    updated

--- monty.h
+++ monty.h
@@ -93,6 +93,7 @@ void pchar(stack_t **stack, unsigned int line_cnt);
 void pstr(stack_t **stack, unsigned int line_cnt);
 void rotl(stack_t **stack, unsigned int line_count);
 void rotr(stack_t **stack, unsigned int line_count);
+
 void opcode(stack_t **stack, char *str, unsigned int line_cnt);
 
 int is_digit(char *string);
